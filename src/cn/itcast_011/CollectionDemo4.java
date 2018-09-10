21bc3bde1487865ffaaa3feec7005c4e0e69f51c
jly <1210056515@qq.com>
2018-09-10 22:32:34 +0800

    20180910w

--- src/cn/itcast_011/CollectionDemo4.java
+++ src/cn/itcast_011/CollectionDemo4.java
@@ -2,6 +2,7 @@ package cn.itcast_011;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 
 public class CollectionDemo4 {
 	/**
@@ -13,7 +14,14 @@ public class CollectionDemo4 {
 	 * 创建学生对象
 	 * 把学生添加到集合
 	 * 把集合转成数组
-	 * 遍历数组
+	 * 遍历数组 可以用while 也可以用for for的效率高
+	 * a.通过集合对象获取迭代器对象
+	 * b.通过迭代器对象的hasNext()方法判断是否有元素
+	 * c.通过迭代器对象的next方法获取元素并移动到下一个位置
+	 * 不要多次使用it.next()方法，因为每次使用都是访问一个对象
+	 * 迭代器：是遍历集合的一种方式
+	 * 迭代器是依赖于集合而存在的
+	 * 
 	 */
 	public static void main(String[] args) {
 		
@@ -36,11 +44,19 @@ public class CollectionDemo4 {
 		Object[] objects = collection.toArray();//向上转型
 		
 		//遍历数组
-		for(int x=0;x<objects.length;x++) {
-//			System.out.println(objects[x]);
-			Student student = (Student)objects[x]; //向下转型
-			System.out.println(student.getName()+"----"+student.getAge());
+		Iterator it = collection.iterator();
+		while(it.hasNext()) {
+			Student s = (Student)it.next();
+			System.out.println(s.getName()+"----"+s.getAge());
 		}
+		
+		System.out.println("----------------");
+		
+		//for循环改写
+//		for(Iterator it=collection.iterator();it.hasNext();) {
+//			Student s = (Student)it.next();
+//			System.out.println(s.getName()+"----"+s.getAge());
+//		}
 
 		
 
